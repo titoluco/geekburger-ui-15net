a4d61532fc32e2e5b6d0dc37e325ee19824163d3
Willian Noronha <willian.noronha@outlook.com>
2019-11-17 13:00:13 -0300

    Add classes para roteiro de teste

---
+++ GeekBurger.UI.Tests/ReceiveMessagesServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeekBurger.UI.Tests
+{
+    public class ReceiveMessagesServiceTests
+    {
+        [Test]
+        public void HandleStoreCatalogReady_Test()
+        {
+
+        }
+    }
+}
---
+++ GeekBurger.UI.Tests/ShowDisplayServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeekBurger.UI.Tests
+{
+    public class ShowDisplayServiceTests
+    {
+        [Test]
+        public void SendMessagesAsync_Test()
+        {
+
+        }
+
+        [Test]
+        public void AddMessage_Test()
+        {
+
+        }
+    }
+}
